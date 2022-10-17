abda943042d423daf2a62454054fe510c1220421
Douglas Navarrete <dranzersv@gmail.com>
2022-10-17 16:12:15 -0600

    added the slider animation to the menu

--- menu.js
+++ menu.js
@@ -1,19 +1,23 @@
 let icono = document.getElementById("icono-menu")
 let menuMobile=document.getElementById("menu-mobile")
-let mostrando = false
+
+
 
 
 
 function alternar(){
 
-    menuMobile.classList.toggle('active')
+    menuMobile.classList.toggle('hide')
 
 }
 function ocultar(){
-    if(window.innerWidth>768){
+    if(window.innerWidth > 1024){
         menuMobile.style.display="none"
-        mostrando=false
+        
+    }else if(window.innerWidth < 1024){
+        menuMobile.style.display="flex"
     }
 }
 icono.addEventListener("click", alternar)
 window.addEventListener("resize", ocultar)
+
